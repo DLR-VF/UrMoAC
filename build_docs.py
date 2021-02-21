3cd8e2f817561603be46268a48639e720a332302
dkrajzew <d.krajzewicz@gmail.com>
2021-02-21 22:05:09 +0100

    building a ststic documentation

---
+++ build_docs.py
@@ -0,0 +1,17 @@
+import os, shutil
+
+USER_PATH = "c:\\users\\dkrajzew\\appdata\\roaming\\python\\python38\\site-packages\\"
+
+os.chdir("docs")
+try:
+    shutil.rmtree("UrMoAC.wiki")
+except OSError as e:
+    print("Error: %s : %s" % ("UrMoAC.wiki", e.strerror))
+try:
+    shutil.rmtree("site")
+except OSError as e:
+    print("Error: %s : %s" % ("site", e.strerror))
+exit()
+os.system("git clone https://github.com/DLR-VF/UrMoAC.wiki.git")
+os.system("python "+USER_PATH+"mkdocs build")
+os.system("python "+USER_PATH+"mkdocs serve")
