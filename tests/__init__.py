bb35b078bf6c3ab614ffd336de39a1da4df720e3
dkrajzew <d.krajzewicz@gmail.com>
2022-07-19 16:36:44 +0200

    adding an init module for further tests

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# =========================================================
+# __init__.py
+# @author Daniel Krajzewicz
+# @date 2022
+# @copyright Institut fuer Verkehrsforschung, 
+#            Deutsches Zentrum fuer Luft- und Raumfahrt
+# @brief Test init module
+# =========================================================
