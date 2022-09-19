0dfdc59a6ba3505bec2d2318374e184c9d9e084e
motaa <alain.schengen@gmail.com>
2022-09-19 10:06:17 +0200

    included Springboot Starter Web and Actuator to POM.

---
+++ src/de/dlr/ivf/urmo/UrMoAcSpringbootApplication.java
@@ -0,0 +1,13 @@
+package de.dlr.ivf.urmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrMoAcSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrMoAcSpringbootApplication.class, args);
+    }
+
+}
