21c921a4ee689eb33cb76d35a21af25c2d036d57
Daniel Krajzewicz <d.krajzewicz@gmail.com>
2023-06-23 14:28:06 +0200

    refactoring

--- tools/visualisation/plot_areas.py
+++ tools/visualisation/plot_areas.py
@@ -76,45 +76,26 @@ def colorbar_index(ncolors, cmap, ticklabels):
   
   
 
-def plot_area_contours(shapel, obj2pos, obj2val, colmap, bounds, shapes2=None, title=None, figsize=(8,5), invalidColor="azure"):
+def plot_area_contours(fig, ax, obj2pos, obj2val, colmap, invalidColor="azure"):
     # compute points
     xs = []
     ys = []
     zs = []
     for g in obj2pos:
-        if g in obj2val: # !!! warn
-            xs.append(obj2pos[g]._shape[0])
-            ys.append(obj2pos[g]._shape[1])
-            zs.append(min(obj2val[g], 900)) # !!!
-    xi = numpy.linspace(min(xs), max(xs), 1000)
-    yi = numpy.linspace(min(ys), max(ys), 1000)
+        if g not in obj2val:
+            continue
+        xs.append(obj2pos[g]._shape[0])
+        ys.append(obj2pos[g]._shape[1])
+        zs.append(min(obj2val[g], 900)) # !!!
+    xi = numpy.linspace(min(xs), max(xs), 1000) # !!!
+    yi = numpy.linspace(min(ys), max(ys), 1000) # !!!
     xi, yi = numpy.meshgrid(xi, yi)
     zi = scipy.interpolate.griddata((xs, ys), zs, (xi, yi), method='linear')
-    # open figure
-    fig = matplotlib.pyplot.figure(figsize=figsize)
-    ax = fig.add_subplot(111)
-    # set clip
-    polys = []
-    if shapel is not None:
-        polys.append(shapel.artist(lw=2, fc="none", ec="black", zorder=1000))
-        polys.append(shapel.artist(lw=0, fc=invalidColor, ec="black", zorder=-1))
-        [ax.add_patch(i) for i in polys]
-    # set figure boundaries and axes
     #
     levels = [0, 150, 300, 450, 600, 750, 900, 1200]
     valueMeasure = "s"
-    clip = None
-    if shapel is not None:
-        # https://stackoverflow.com/questions/69268369/how-to-use-set-clip-path-with-multiple-polygons
-        vertices = [i.get_path().vertices for i in polys]
-        vertices = [item for sub_list in vertices for item in sub_list]
-        codes = [i.get_path().codes for i in polys]
-        codes = [item for sub_list in codes for item in sub_list]
-        clip = matplotlib.patches.PathPatch(matplotlib.patches.Path(vertices, codes), transform=ax.transData)
     cs1 = matplotlib.pyplot.contourf(xi, yi, zi, levels=levels, cmap=colmap, zorder=20)
     #cs1 = matplotlib.pyplot.contourf(xi, yi, zi, levels=100, cmap=colmap, zorder=20)
-    if shapes2: 
-        colB = ax.add_collection( PatchCollection( shapes2, facecolors='none', linewidths=1., edgecolor="black", zorder=40  ))
     if clip is not None:
         for col in cs1.collections:
             col.set_clip_path(clip)
@@ -130,47 +111,13 @@ def plot_area_contours(shapel, obj2pos, obj2val, colmap, bounds, shapes2=None, t
     if True:
         cbar = colorbar_index(len(levels[1:]), colmap, labels)#["300s","600s","900s","1200s","1500s","1800s"])
         cbar.ax.tick_params(labelsize=12)   
-    if title is not None: 
-        title = title.replace("\\n", "\n")
-        matplotlib.pyplot.title(title, size=16)
-    matplotlib.pyplot.xlim(bounds[0], bounds[2])
-    matplotlib.pyplot.ylim(bounds[1], bounds[3])
-    ax.grid(False)
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    matplotlib.pyplot.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-    matplotlib.pyplot.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
-    matplotlib.pyplot.gca().xaxis.set_major_locator(matplotlib.pyplot.NullLocator())
-    matplotlib.pyplot.gca().yaxis.set_major_locator(matplotlib.pyplot.NullLocator())
-    ax.set_aspect('equal', 'datalim')
-    matplotlib.pyplot.subplots_adjust(bottom=0.05, right=.96, left=.02, top=0.9, wspace=0)
-    return fig, ax
+    #return fig, ax
 
 
 
-def plot_area_objects(shapel, obj2pos, obj2val, colmap, bounds, shapes2=None, title=None, figsize=(8,5), invalidColor="azure", from_borderwidth=1.):
-    # open figure
-    fig = matplotlib.pyplot.figure(figsize=figsize)
-    ax = fig.add_subplot(111)
-    # set clip
-    polys = []
-    if shapel is not None:
-        polys.append(shapel.artist(lw=2, fc="none", ec="black", zorder=1000))
-        polys.append(shapel.artist(lw=0, fc=invalidColor, ec="black", zorder=-1))
-        [ax.add_patch(i) for i in polys]
-        #[ax.add_artist(i) for i in polys]
-    # set figure boundaries and axes
-    #
+def plot_area_objects(fig, ax, obj2pos, obj2val, colmap, invalidColor="azure", from_borderwidth=1.):
     #levels = [0, 150, 300, 450, 500, 650, 900]
     valueMeasure = "s"
-    clip = None
-    if shapel is not None:
-        # https://stackoverflow.com/questions/69268369/how-to-use-set-clip-path-with-multiple-polygons
-        vertices = [i.get_path().vertices for i in polys]
-        vertices = [item for sub_list in vertices for item in sub_list]
-        codes = [i.get_path().codes for i in polys]
-        codes = [item for sub_list in codes for item in sub_list]
-        clip = matplotlib.patches.PathPatch(matplotlib.patches.Path(vertices, codes), transform=ax.transData)
     sm = matplotlib.cm.ScalarMappable(cmap=colmap, norm=matplotlib.pyplot.Normalize(vmin=0, vmax=900))
     patches = []
     for o in obj2pos:
@@ -182,11 +129,12 @@ def plot_area_objects(shapel, obj2pos, obj2val, colmap, bounds, shapes2=None, ti
         if p is None:
             print ("Missing geometry for object %s" % o)
             continue
+        """
         if clip is not None:
+            print("Here")
             p.set_clip_path(clip)
+        """
         ax.add_artist(p)
-    if shapes2: 
-        colB = ax.add_collection( PatchCollection( shapes2, facecolors='none', linewidths=1., edgecolor="black", zorder=40  ))
     # fake up the array of the scalar mappable. Urgh..." (pelson, http://stackoverflow.com/questions/8342549/matplotlib-add-colorbar-to-a-sequence-of-line-plots)
     sm._A = []
     labels = []
@@ -194,21 +142,6 @@ def plot_area_objects(shapel, obj2pos, obj2val, colmap, bounds, shapes2=None, ti
         cbar = matplotlib.pyplot.colorbar(sm, ticks=range(0, 1000, 100))
         cbar.ax.tick_params(labelsize=12)   
         cbar.ax.set_yticklabels(["0 s", "100 s", "200 s", "300 s", "400 s", "500 s", "600 s", "700 s", "800 s", ">= 900 s"])
-    if title is not None: 
-        title = title.replace("\\n", "\n")
-        matplotlib.pyplot.title(title, size=16)
-    matplotlib.pyplot.xlim(bounds[0], bounds[2])
-    matplotlib.pyplot.ylim(bounds[1], bounds[3])
-    ax.grid(False)
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    matplotlib.pyplot.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-    matplotlib.pyplot.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
-    matplotlib.pyplot.gca().xaxis.set_major_locator(matplotlib.pyplot.NullLocator())
-    matplotlib.pyplot.gca().yaxis.set_major_locator(matplotlib.pyplot.NullLocator())
-    ax.set_aspect('equal', 'datalim')
-    matplotlib.pyplot.subplots_adjust(bottom=0.05, right=.96, left=.02, top=0.9, wspace=0)
-    return fig, ax
 
 
 
@@ -287,51 +220,85 @@ def load_measures(source, measure):
 
 if __name__ == "__main__":
     options, remaining_args = parse_options()
+    
+    # -- load objects
     # load the main border optionally
     mainBorder = None
     if options.mainBorder!=None:
-        mainBorder = load_shapes(options.mainBorder, options.projection, False)
-        mainBorder = mainBorder[list(mainBorder.keys())[0]]
+        shapes = load_shapes(options.mainBorder, options.projection, False)
+        mainBorder = shapes[list(shapes.keys())[0]] # use only the first geometry
     # load the inner borders optionally
-    innerBorders = None
-    if options.innerBorders!=None:
-        innerBorders = load_shapes(options.innerBorders, options.projection, False)
+    innerBorders = load_shapes(options.innerBorders, options.projection, False) if options.innerBorders is not None else None
     # load the network optionally
-    network = None
-    if options.net:
-        network = net.loadNet("postgresql:"+options.net, options.projection, ["street_type"])
-    # load the water
-    water = None
-    if options.water:
-        water = load_shapes(options.water, options.projection, False)
-    # get the optional bounding box
+    network = net.loadNet("postgresql:"+options.net, options.projection, ["street_type"]) if options.net is not None else None
+    # load the water optionally
+    water = load_shapes(options.water, options.projection, False) if options.water is not None else None
+    # load shapes and measures
+    obj2geom = load_shapes(options.objects, options.projection, options.contour, options.objectsID, options.objectsGeom, options.objectsFilter)
+    obj2value = load_measures(options.measures, options.measuresValue)
+    
+    # -- draw
+    # open figure
+    figsize = (8,5)
+    fig = matplotlib.pyplot.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    #
+    invalidColor = "azure"
+    colormap = matplotlib.pyplot.get_cmap(options.colmap)
+    # build clip
+    clip = mainBorder.artist(lw=2, fc="red", ec="black", transform=ax.transData) if mainBorder is not None else None
+    # draw
+    if options.contour:
+        plot_area_contours(fig, ax, obj2geom, obj2value, colormap)
+    else:
+        plot_area_objects(fig, ax, obj2geom, obj2value, colormap, from_borderwidth=options.from_borderwidth)
+    if water is not None:
+        for w in water:
+            p = water[w].artist(fc="#3cacd5", lw=0, zorder=900)
+            ax.add_artist(p)
+    if innerBorders is not None:
+        ax.add_collection(PatchCollection(innerBorders, facecolors='none', linewidths=1., edgecolor="black", zorder=40  ))
+    if network is not None:
+        net.plotNet(ax, network, color="#000000", alpha=1)
+    # apply clipping
+    for o in ax.get_children():
+        o.set_clip_path(clip)
+    # add bounds after clipping
+    if mainBorder is not None:
+        ax.add_patch(mainBorder.artist(lw=2, fc="none", ec="black", zorder=1000))
+        ax.add_patch(mainBorder.artist(lw=0, fc=invalidColor, ec="black", zorder=-1))
+    # decorate
+    if options.title is not None: 
+        matplotlib.pyplot.title(title.replace("\\n", "\n"), size=16)
+    
+    # -- get/set bounds
+    # compute the boundary to show if not given
     bounds = None
     if options.bounds:
         bounds = [float(v) for v in options.bounds.split(",")]
         if len(bounds)!=4:
             print ("The bounds must be a tuple of minx,miny,maxx,maxy.")
             exit()
-    # load shapes and measures
-    obj2geom = load_shapes(options.objects, options.projection, options.contour, options.objectsID, options.objectsGeom, options.objectsFilter)
-    obj2value = load_measures(options.measures, options.measuresValue)
-    # compute the boundary to show if not given
-    if bounds is None:
+    else:
         if mainBorder is not None:
             bounds = mainBorder.bounds()
         else:
             bounds = spatialhelper.geometries_bounds(list(obj2pos.values()))
-    # draw
-    colormap = matplotlib.pyplot.get_cmap(options.colmap)
-    if options.contour:
-        fig, ax = plot_area_contours(mainBorder, obj2geom, obj2value, colormap, bounds, innerBorders, title=options.title)
-    else:
-        fig, ax = plot_area_objects(mainBorder, obj2geom, obj2value, colormap, bounds, innerBorders, title=options.title, from_borderwidth=options.from_borderwidth)
-    if water is not None:
-        for w in water:
-            p = water[w].artist(fc="#3cacd5", lw=0, zorder=900)
-            ax.add_artist(p)
-    if network is not None:
-        net.plotNet(ax, network, color="#000000", alpha=1)
+    matplotlib.pyplot.xlim(bounds[0], bounds[2])
+    matplotlib.pyplot.ylim(bounds[1], bounds[3])
+    
+    # -- set scaling and axes
+    ax.grid(False)
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    matplotlib.pyplot.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+    matplotlib.pyplot.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
+    matplotlib.pyplot.gca().xaxis.set_major_locator(matplotlib.pyplot.NullLocator())
+    matplotlib.pyplot.gca().yaxis.set_major_locator(matplotlib.pyplot.NullLocator())
+    ax.set_aspect('equal', 'datalim')
+    matplotlib.pyplot.subplots_adjust(bottom=0.05, right=.96, left=.02, top=0.9, wspace=0)
+
+    # show / save
     if options.output is not None:
         matplotlib.pyplot.savefig(options.output)
     if not options.no_show:
