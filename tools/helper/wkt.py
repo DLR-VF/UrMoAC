4c44792d106ceef495c3c6b6c2e593ac8c1bfc3c
Daniel Krajzewicz <d.krajzewicz@gmail.com>
2025-03-24 16:56:35 +0100

    debugging and beautifying

--- tools/helper/wkt.py
+++ tools/helper/wkt.py
@@ -27,7 +27,7 @@ class GeometryType(IntEnum):
     """An enumeration of known geometry types"""
     POINT = 0
     LINESTRING = 1
-    MULTILINE = 2
+    MULTILINESTRING = 2
     POLYGON = 3
     MULTIPOLYGON = 4
     GEOMETRYCOLLECTION = 5
@@ -112,14 +112,12 @@ def encode_complex_polygon(polys, close):
 
 
 
-
 # --- class definitions -----------------------------------------------------
 class Geometry:
     """An abstract geometry"""
 
     def __init__(self, type, shape):
         """ Constructor
-    
         
         @param type The type of the geometry
         @param shape The geometry representation
@@ -130,18 +128,17 @@ class Geometry:
     
     def artist(self):
         """Returns a matplotlib artist that represents this geometry"""
-        raise ValueError("abstract Geometry type")
+        raise ValueError("abstract Geometry type") # pragma: no cover
 
 
     def bounds(self):
         """Returns the bounds of this geometry"""
-        raise ValueError("abstract Geometry type")
+        raise ValueError("abstract Geometry type") # pragma: no cover
 
 
     def shape(self):
         """Returns the shape of this geometry"""
         return self._shape
-
 
 
 
@@ -185,7 +182,6 @@ class LineString(Geometry):
         import matplotlib.path
         path = matplotlib.path.Path(self._shape, closed=False)
         return matplotlib.patches.PathPatch(path, fill=False, **kwargs)
-
 
 
     def bounds(self):
@@ -234,21 +230,27 @@ class Polygon(Geometry):
         return bounds
 
 
-
+    def wkt(self):
+        d = []
+        for poly in self._shape:
+            npoly = ["%s %s" % (p[0], p[1]) for p in poly]
+            d.append("(" + ",".join(npoly) + ")")
+        d = ",".join(d)
+        return "'POLYGON(" + d + ")'"
 
 
     
 
 
-class MultiLine(Geometry):
-    """A multiline"""
+class MultiLineString(Geometry):
+    """A multilinestring"""
 
     def __init__(self, shape):
         """ Constructor
         
         @param shape The geometry representation
         """
-        Geometry.__init__(self, GeometryType.MULTILINE, shape)
+        Geometry.__init__(self, GeometryType.MULTILINESTRING, shape)
 
 
     def artist(self, **kwargs):
@@ -337,17 +339,17 @@ def parse_LINESTRING2D(which):
     """
     if which.find("EMPTY")>0:
         return None
-    which = which[which.find("("):which.rfind(")")+1]
+    which = which[which.find("(")+1:which.rfind(")")]
     which = which.split(",")
     cline = []
     for p in which:
-        xy = p.split(" ")
+        xy = p.strip().split(" ")
         cline.append( [ float(xy[0]), float(xy[1]) ] )
     return cline
 
 
-def parse_MULTILINE2D(which):
-    """Parses the given geometry assuming it's a 2D MULTILINE
+def parse_MULTILINESTRING2D(which):
+    """Parses the given geometry assuming it's a 2D MULTILINESTRING
     
     @return The parsed geometry list of position lists
     """
@@ -365,7 +367,7 @@ def parse_MULTILINE2D(which):
         for p in line:
             if len(p.strip())==0:
                 continue
-            xy = p.split(" ")
+            xy = p.strip().split(" ")
             cpoints.append( [ float(xy[0]), float(xy[1]) ] )
         clines.append(cpoints)
     return clines
@@ -442,19 +444,13 @@ def parse_MULTIPOLYGON2D(which):
 
 
 
-
-
-
-
-
-
-
 def wkt2geometry(wkt):
     """Parses the given WKT into a Geometry object
   
     @param wkt The WKT
     @return The Geometry representing the WKT
     """
+    wkt = wkt.upper()
     if wkt.startswith("POINT"):
         shape = parse_POINT2D(wkt)
         if shape==None: return None
@@ -463,10 +459,10 @@ def wkt2geometry(wkt):
         shape = parse_LINESTRING2D(wkt)
         if shape==None: return None
         return LineString(shape)
-    elif wkt.startswith("MULTILINE"):
-        shape = parse_MULTILINE2D(wkt)
+    elif wkt.startswith("MULTILINESTRING"):
+        shape = parse_MULTILINESTRING2D(wkt)
         if shape==None: return None
-        return MultiLine(shape)
+        return MultiLineString(shape)
     elif wkt.startswith("POLYGON"):
         shape = parse_POLYGON2D(wkt)
         if shape==None: return None
@@ -485,16 +481,18 @@ def wkt2lists(wkt):
     @param wkt The WKT
     @return Lists of different dimensions in dependence of the WKT type
     """
+    wkt = wkt.upper()
     if wkt.startswith("POINT"):
         return parse_POINT2D(wkt)
     elif wkt.startswith("LINESTRING"):
         return parse_LINESTRING2D(wkt)
-    elif wkt.startswith("MULTILINE"):
-        return parse_MULTILINE2D(wkt)
+    elif wkt.startswith("MULTILINESTRING"):
+        return parse_MULTILINESTRING2D(wkt)
     elif wkt.startswith("POLYGON"):
         return parse_POLYGON2D(wkt)
     elif wkt.startswith("MULTIPOLYGON"):
         return parse_MULTIPOLYGON2D(wkt)
     else:
         raise ValueError("Unknown geometry '%s'" % wkt)
+
 
